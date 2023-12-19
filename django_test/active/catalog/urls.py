d1ba9f468576350c49ef7bb0088027a626141317
jolivald <jolivald.jonathan@gmail.com>
2023-12-19 16:57:45 +0100

    created all views plus home

--- django_test/active/catalog/urls.py
+++ django_test/active/catalog/urls.py
@@ -4,8 +4,35 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+
+    path('activite', views.activite_list, name="activite_list"),
+    path('activite/<id>', views.activite_detail, name="activite_detail"),
+    
+    path('client', views.client_list, name="client_list"),
+    path('client/<id>', views.client_detail, name="client_detail"),
+
     path('collaborateur', views.collaborateur_list, name="collaborateur_list"),
-    path('collaborateur/<matricule>', views.collaborateur_detail, name="collaborateur_detail"),
+    path('collaborateur/<id>', views.collaborateur_detail, name="collaborateur_detail"),
+
+    path('contact', views.contact_list, name="contact_list"),
+    path('contact/<id>', views.contact_detail, name="contact_detail"),
+
+    path('contrat', views.contrat_list, name="contrat_list"),
+    path('contrat/<id>', views.contrat_detail, name="contrat_detail"),
+
+    path('document', views.document_list, name="document_list"),
+    path('document/<id>', views.document_detail, name="document_detail"),
+
+    path('etape', views.etape_list, name="etape_list"),
+    path('etape/<id>', views.etape_detail, name="etape_detail"),
+
     path('fonction', views.fonction_list, name="fonction_list"),
     path('fonction/<id>', views.fonction_detail, name="fonction_detail"),
+
+    path('intervention', views.intervention_list, name="intervention_list"),
+    path('intervention/<id>', views.intervention_detail, name="intervention_detail"),
+
+    path('projet', views.projet_list, name="projet_list"),
+    path('projet/<id>', views.projet_detail, name="projet_detail"),
+
 ]
--- django_test/active/catalog/views.py
+++ django_test/active/catalog/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Collaborateur, Fonction
+from .models import Activite, Client, Collaborateur, Contact, Contrat, Document, Etape, Fonction, Intervention, Projet
 
 def _display_list(request, objects, options):
   tpl = loader.get_template('catalog/list.html')
@@ -25,7 +25,43 @@ def _display_detail(request, title, obj):
   return HttpResponse(tpl.render(ctx, request))
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the catalog index.")
+  tpl = loader.get_template('catalog/home.html')
+  ctx = {
+    'options': { 'title': 'xXx Projet xXx Active xXx' },
+  }
+  return HttpResponse(tpl.render(ctx, request))
+
+
+def activite_list(request):
+  return _display_list(request, Activite.objects.all(), {
+     'title': 'Activités',
+     'detail': 'activite_detail',
+     'primary': 'id_activite'
+  })
+
+def activite_detail(request, id):
+  return _display_detail(
+    request,
+    'Activité',
+    Activite.objects.get(id_activite=id)
+  )
+
+
+def client_list(request):
+  return _display_list(request, Client.objects.all(), {
+     'title': 'Cients',
+     'detail': 'client_detail',
+     'primary': 'code_client'
+  })
+
+def client_detail(request, id):
+  return _display_detail(
+    request,
+    'Client',
+    Client.objects.get(code_client=id)
+  )
+
+
 
 def collaborateur_list(request):
   return _display_list(request, Collaborateur.objects.all(), {
@@ -34,13 +70,73 @@ def collaborateur_list(request):
      'primary': 'matricule'
   })
 
-
-def collaborateur_detail(request, matricule):
+def collaborateur_detail(request, id):
   return _display_detail(
     request,
     'Collaborateur',
-    Collaborateur.objects.get(matricule=matricule)
+    Collaborateur.objects.get(matricule=id)
   )
+
+
+def contact_list(request):
+  return _display_list(request, Contact.objects.all(), {
+     'title': 'Contacts',
+     'detail': 'contact_detail',
+     'primary': 'id_contact'
+  })
+
+def contact_detail(request, id):
+  return _display_detail(
+    request,
+    'Contact',
+    Contact.objects.get(id_contact=id)
+  )
+
+
+def contrat_list(request):
+  return _display_list(request, Contrat.objects.all(), {
+     'title': 'Contrats',
+     'detail': 'contrat_detail',
+     'primary': 'id_contrat'
+  })
+
+def contrat_detail(request, id):
+  return _display_detail(
+    request,
+    'Contrat',
+    Contrat.objects.get(id_contrat=id)
+  )
+
+
+def document_list(request):
+  return _display_list(request, Document.objects.all(), {
+     'title': 'Documents',
+     'detail': 'document_detail',
+     'primary': 'id_document'
+  })
+
+def document_detail(request, id):
+  return _display_detail(
+    request,
+    'Document',
+    Document.objects.get(id_document=id)
+  )
+
+
+def etape_list(request):
+  return _display_list(request, Etape.objects.all(), {
+     'title': 'Etapes',
+     'detail': 'etape_detail',
+     'primary': 'num_lot'
+  })
+
+def etape_detail(request, id):
+  return _display_detail(
+    request,
+    'Etape',
+    Etape.objects.get(num_lot=id)
+  )
+
 
 def fonction_list(request):
   return _display_list(request, Fonction.objects.all(), {
@@ -55,5 +151,35 @@ def fonction_detail(request, id):
     request,
     'Fonction',
     Fonction.objects.get(id_fonction=id)
+  )
+
+
+def intervention_list(request):
+  return _display_list(request, Intervention.objects.all(), {
+     'title': 'Interventions',
+     'detail': 'intervention_detail',
+     'primary': 'id_intervention'
+  })
+
+def intervention_detail(request, id):
+  return _display_detail(
+    request,
+    'Intervention',
+    Intervention.objects.get(id_intervention=id)
+  )
+
+
+def projet_list(request):
+  return _display_list(request, Projet.objects.all(), {
+     'title': 'Projets',
+     'detail': 'projet_detail',
+     'primary': 'code_projet'
+  })
+
+def projet_detail(request, id):
+  return _display_detail(
+    request,
+    'Projet',
+    Projet.objects.get(code_projet=id)
   )
 
